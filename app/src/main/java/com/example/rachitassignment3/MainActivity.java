49e70358f0b0a7eabc3342d579898c295b48029a
Rachit.Sharma <rachit.sharma@ibm.com>
2020-10-05 13:26:17 -0400

    New Push
    -Updated the adapter class
    -added the viwe objects

--- app/src/main/java/com/example/rachitassignment3/MainActivity.java
+++ app/src/main/java/com/example/rachitassignment3/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.rachitassignment3;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
@@ -21,5 +22,11 @@ public class MainActivity extends AppCompatActivity {
 
         s1 = getResources().getStringArray(R.array.recipes);
         s1 = getResources().getStringArray(R.array.descriptions);
+
+        RecyclerViewAdapter myAdapter = new RecyclerViewAdapter(this,s1,s2,images);
+        recyclerView.setAdapter(myAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+
     }
 }
--- app/src/main/java/com/example/rachitassignment3/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/rachitassignment3/RecyclerViewAdapter.java
@@ -1,33 +1,63 @@
 package com.example.rachitassignment3;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.MyViewHolder> {
+    private static final String TAG = "RecyclerViewAdapter";
+
+    String data1[], data2[];
+    int images[];
+    Context context;
+
+    public RecyclerViewAdapter(Context ct, String s1[], String s2[], int img[]){
+        context = ct;
+        data1 = s1;
+        data2 = s2;
+        images = img;
+
+    }
 
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.my_row, parent, false);
+        return new MyViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        holder.myText1.setText(data1[position]);
+        holder.myText2.setText(data2[position]);
+        holder.myImage.setImageResource(images[position]);
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return images.length;
     }
 
     public class MyViewHolder extends RecyclerView.ViewHolder {
 
+        TextView myText1, myText2;
+        ImageView myImage;
+
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
+            myText1 = itemView.findViewById(R.id.myRecipeText);
+            myText2 = itemView.findViewById(R.id.myDescText);
+            myImage = itemView.findViewById(R.id.myImageView);
+
         }
     }
 }
